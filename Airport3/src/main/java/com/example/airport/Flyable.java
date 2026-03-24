35c07538d67a4a1aaf8e10b7c456c2ac404266e2
Jack12231231 <gleb28032007@gmail.com>
2026-03-24 21:28:13 +0300

    Delete Airport3 directory

--- Airport3/src/main/java/com/example/airport/Flyable.java
+++
@@ -1,5 +0,0 @@
-package com.example.airport;
-
-public interface Flyable {
-    void fly();
-}
--- Airport3/src/main/java/com/example/airport/Maintainable.java
+++
@@ -1,5 +0,0 @@
-package com.example.airport;
-
-public interface Maintainable {
-    void performMaintenance();
-}
